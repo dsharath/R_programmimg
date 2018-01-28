90a3bebdf56be3210eecd64140ea9c003f12b2e4
dsharath <dsharathc7@gmail.com>
2018-01-27 18:20:00 -0600

    doc: Adding quiz Q&A

---
+++ lesson_08/price_vs_carat_and_cut.r
@@ -0,0 +1,18 @@
+# Let’s look at cut and see if we find a similar result.
+
+# Adjust the code below to color the points by cut.
+# Change any other parts of the code as needed.
+
+# ALTER THE CODE BELOW THIS LINE
+# =========================================================================================
+
+ggplot(aes(x = carat, y = price, color = cut), data = diamonds) + 
+  geom_point(alpha = 0.5, size = 1, position = 'jitter') +
+  scale_color_brewer(type = 'div',
+                     guide = guide_legend(title = 'Clarity', reverse = T,
+                                          override.aes = list(alpha = 1, size = 2))) +  
+  scale_x_continuous(trans = cuberoot_trans(), limits = c(0.2, 3),
+                     breaks = c(0.2, 0.5, 1, 2, 3)) + 
+  scale_y_continuous(trans = log10_trans(), limits = c(350, 15000),
+                     breaks = c(350, 1000, 5000, 10000, 15000)) +
+  ggtitle('Price (log10) by Cube-Root of Carat and Clarity')
